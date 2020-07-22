c11a468473eb5a0a39c2f3efa45b173ba1435ac4
chrismgonzalez <cmgonza89@gmail.com>
2020-07-22 13:51:57 -0500

    solved 2 problems

--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,11 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+	result := 0
+	for _, v := range numbers {
+		result += v
+	}
+	return result
 }
+
+// todo - implement a recursive solution
